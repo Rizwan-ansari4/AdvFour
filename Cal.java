7a26cbee40046e8d530ce1d279663d46cc4041e6
rizwan_ansari4 <rizwanansaricode@gamil.com>
2024-10-27 12:44:19 +0530

    developed two functions that is add and multiplication

---
+++ Cal.java
@@ -0,0 +1,9 @@
+class Cal{
+	public static double add(double num1, double num2){
+		return num1 + num2;
+	}
+
+	public static double multiple(double num1, double num2){
+		return num1 * num2;
+	}
+}
